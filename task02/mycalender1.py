6bb81d9767e8dbd729d62a0f54ffe012ea9bc86e
aditir360 <71522856+aditir360@users.noreply.github.com>
2020-09-19 13:45:14 -0700

    Update mycalender1.py

--- task02/mycalender1.py
+++ task02/mycalender1.py
@@ -5,13 +5,13 @@
 #
 
 def main():
-    my_calendar1 = "Day             Time          Subject
+    my_calendar1 = """Day             Time          Subject
                     Monday   9:10 AM - 10:15 AM      LA
                               10:35 AM - 11:40 AM     SS
                               12:10 PM - 1:15 PM      S
                     Tuesday  9:10 AM - 10:15 AM     Math
                               10:35 AM - 11:40 AM  Orchestra
-                              12:10 PM - 1:15 PM      PF"
+                              12:10 PM - 1:15 PM      PF"""
     # This is a string. After wards I will just print it.
     print(my_calendar1)
     # I printed the string.
