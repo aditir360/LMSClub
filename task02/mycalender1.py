ab4a42c9fdddc1a2b09ec8daf5f97c56e3aa52aa
Aditi Ranjan <aditi.ranjan.mtv@gmail.com>
2020-09-18 20:20:32 -0700

    This is my first calendar program of school.

---
+++ task02/mycalender1.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env pytho3
+
+# 
+# This is my program to make my own calendar.
+#
+
+def main():
+    my_character1 = "Day             Time          Subject
+                    Monday   9:10 AM - 10:15 AM      LA
+                              10:35 AM - 11:40 AM     SS
+                              12:10 PM - 1:15 PM      S
+                    Tuesday  9:10 AM - 10:15 AM     Math
+                              10:35 AM - 11:40 AM  Orchestra
+                              12:10 PM - 1:15 PM      PF"
+    # This is a string. After wards I will just print it.
+    print(my_character1)
+    # I printed the string.
+
+
+
+
+if __name__ == "__main__":
+    main()
