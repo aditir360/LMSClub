b92f3d9a82dc93c70832a2a570b1cabacf25e35e
aditir360 <71522856+aditir360@users.noreply.github.com>
2020-09-18 21:02:53 -0700

    My mycalendar1.py file.
    This is my mycalendar1 program using a function.

---
+++ task02/mycalendar1_function.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+#
+# This is my mycalendar1 program using a function.
+#
+
+def my_calendar1(mycalendar1):
+    mycalendar1 = "Day             Time          Subject
+                    Monday   9:10 AM - 10:15 AM      LA
+                              10:35 AM - 11:40 AM     SS
+                              12:10 PM - 1:15 PM      S
+                    Tuesday  9:10 AM - 10:15 AM     Math
+                              10:35 AM - 11:40 AM  Orchestra
+                              12:10 PM - 1:15 PM      PF"
+    return mycalendar1
+
+def main():
+    print(my_calendar1(mycalendar1))
+
