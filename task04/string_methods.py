d4d92303da00c0013f28074ffe5ed7b3371c8564
aditir360 <71522856+aditir360@users.noreply.github.com>
2020-09-21 20:27:32 -0700

    This is my code thats states proof that I have read about strings. This includes functions used with strings.
    This is my code which includes functions that apply for strings.

--- task04/string_methods.py
+++ task04/string_methods.py
@@ -9,17 +9,17 @@ def main():
     # FIRST EXAMPLE
     txt = "Python is an awesome and easy Programming Language to learn!"
     # My txt.
-    txt_using_function = txt.casefold()
+    txt_using_function_task04 = txt.casefold()
     # This is where I apply a function that will make all letters in the txt lowercase.
-    print(txt_using_function)
+    print(txt_using_function_task04)
     # I printed the txt_using_function, to see the result.
     
     # SECOND EXAMPLE
     txt1 = "python is an awesome and easy programming language to learn!"
     # My txt.
-    txt1_using_function = txt1.capitilize()
+    txt1_using_function_task04 = txt1.capitilize()
     # This is where I apply a function that will capitilize the first letter in the txt.
-    print(txt1_using_function)
+    print(txt1_using_function_task04)
     # I printed the txt1_using_function, to see the result.
     
     
