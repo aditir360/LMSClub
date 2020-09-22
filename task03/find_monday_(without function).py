37c3306a7030a725887a977a959a619ca689f99b
aditir360 <71522856+aditir360@users.noreply.github.com>
2020-09-21 18:07:09 -0700

    task03
    This is my code which includes a function that will check if in my schedule that there is a "Monday" in it, and prints the schedule's length.

---
+++ task03/find_monday_(without function).py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+#
+# This is my code that will check if in my schedule that there is a "Monday" in it without a function, and prints the schedule's length.
+#
+
+
+def main():
+    mycalendar1 =  """Day               Time          Subject
+                    Monday   9:10 AM - 10:15 AM      LA
+                              10:35 AM - 11:40 AM     SS
+                              12:10 PM - 1:15 PM      S
+                    Tuesday  9:10 AM - 10:15 AM     Math
+                              10:35 AM - 11:40 AM  Orchestra
+                              12:10 PM - 1:15 PM      PF"""
+   # This is my calendar.
+   if "Monday" in mycalendar1:
+       print "Monday is present."
+   else:
+       print "Monday is not present."
+   # This is how Python will check if "Monday" is there in the schedule or not.
+   return(without a function).
+   
+   print len(mycalendar1)
+   # This is where I print my schedule's length using the len() function.
