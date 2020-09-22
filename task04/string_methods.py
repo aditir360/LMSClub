a83abce49ec8659b14ec32984a30a630d392cd3c
aditir360 <71522856+aditir360@users.noreply.github.com>
2020-09-21 18:23:39 -0700

    task04
    This is my code which includes functions that apply for strings.

---
+++ task04/string_methods.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+#
+# This is my code which includes functions that apply for strings.
+#
+
+
+def main():
+    # FIRST EXAMPLE
+    txt = "Python is an awesome and easy Programming Language to learn!"
+    # My txt.
+    txt_using_function = txt.casefold()
+    # This is where I apply a function that will make all letters in the txt lowercase.
+    print(txt_using_function)
+    # I printed the txt_using_function, to see the result.
+    
+    # SECOND EXAMPLE
+    txt1 = "Python is an awesome and easy Programming Language to learn!"
+    # My txt.
+    txt1_using_function = txt1.capitiliaze()
+    # This is where I apply a function that will capitilize the first letter in the txt.
+    print(txt1_using_function)
+    # I printed the txt1_using_function, to see the result.
+    
+    
+    
+    
